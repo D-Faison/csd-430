c3846c4fba82de6e56c40dc854e1d7ad77f45dfb
D-Faison <dfaison2017@gmail.com>
2025-09-06 17:58:33 -0400

    update

---
+++ module-5_6/Project Part 1/faison56ProjectPart1/src/main/java/dbBeans/DBbean.java
@@ -0,0 +1,117 @@
+
+package dbBeans;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+public class DBbean implements java.io.Serializable {
+    //Establish setters and getters for movies
+    private int id;
+    private String movieTitle;
+    private int releaseYear;
+    private String runTime;
+    private String director;
+    private double rating;
+    private String genre;
+    //Connection Variables
+    private String url ="jdbc:mysql://localhost:3306/csd340";
+    private String user = "student1";
+    private String password = "pass";
+    
+    //ID
+    public int getID(){
+        return id;
+    }
+    public void setID(int id){
+        this.id = id;
+    }
+    //Movie Title
+    public String getMovieTitle(){
+        return movieTitle;
+    }
+    public void setMovieTitle(String movieTitle){
+        this.movieTitle = movieTitle;
+    }
+    //Release Year
+    public int getReleaseYear(){
+        return releaseYear;
+    }
+    public void setReleaseYear(int releaseYear){
+        this.releaseYear = releaseYear;
+    }
+    //Run Time
+    public String getRunTime(){
+        return runTime;
+    }
+    public void setRunTime(String runTime){
+        this.runTime = runTime;
+    }
+    //Director
+    public String getDirector(){
+        return director;
+    }
+    public void setDirector(String director){
+        this.director = director;
+    }
+    //Rating
+    public double getRating(){
+        return rating;
+    }
+    public void setRating(double rating){
+        this.rating = rating;
+    }
+    //Genre
+    public String getGenre(){
+        return genre;
+    }
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+    
+    public ArrayList<Integer> fetchAllIDs(){
+        ArrayList<Integer> movieIDs = new ArrayList<>();
+        try(Connection conn = DriverManager.getConnection(url,user,password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT id FROM dejanae_movies_data")){
+            
+            while(rs.next()){
+                movieIDs.add(rs.getInt("id"));
+            }
+        
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.print("Error fetching table");
+        }
+        
+        return movieIDs;
+    }
+    
+    public DBbean getMovieByID(int id){
+        DBbean movie = null;
+        try( Connection conn = DriverManager.getConnection(url,user,password);
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM dejanae_movies_data WHERE ID = ?") ){
+            
+           pstmt.setInt(1, id);
+           try(ResultSet rs = pstmt.executeQuery()){
+               if(rs.next()){
+                   movie = new DBbean();
+                   movie.setID(rs.getInt("ID"));
+                   movie.setMovieTitle(rs.getString("MovieTitle"));
+                   movie.setReleaseYear(rs.getInt("ReleaseYear"));
+                   movie.setRunTime(rs.getString("RunTime"));
+                   movie.setDirector(rs.getString("Director"));
+                   movie.setRating(rs.getDouble("Rating"));
+                   movie.setGenre(rs.getString("Genre"));
+               }
+           
+           }
+        
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.print("Error fetching ID");
+        }
+        return movie;
+    }
+    
+}
---
+++ module-5_6/Project Part 1/faison56ProjectPart1/src/main/webapp/index.jsp
@@ -0,0 +1,35 @@
+<%--DeJanae Faison M5&6 Assignment--%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import = "dbBeans.DBbean" %>
+<%@page import = "java.util.ArrayList" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>&#9630; Movie Finder &#9626;</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Concert+One&display=swap');
+        </style>
+        <link rel="stylesheet" href="faison_5_6_style.css">
+    </head>
+    <body>
+        <H1>Movie Finder</H1>
+        <div class="movieInfoBody">
+            <h2>Find your next favorite movie!</h2>
+            <form action="---file here---" method="post">
+                <label for="movieID">Select Movie to view details:</label>
+                <select name="movieID" id="movieID">
+                    <%
+                        DBbean movieBean = new DBbean();
+                        ArrayList<Integer> ids = movieBean.fetchAllIDs();
+                        for(Integer id:ids){
+                    %>
+                    <<option value="<%=id%>"><%=id%></option>
+                    <%}%>
+                </select>
+                <input type="submit" value="View Movie" id="viewMovieButton">
+            </form>
+        </div> 
+    </body>
+</html>
